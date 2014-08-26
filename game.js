12390661c27997a0e42f43f40ab735a536eb0f4f
Taylor Garcia <tclustuff@gmail.com>
2014-08-26 08:49:59 -0600

    Stripped out unneeded stuff from the template

--- game.js
+++ game.js
@@ -7,29 +7,25 @@ var KC_LEFT = 37;
 var KC_UP = 38;
 var KC_RIGHT = 39;
 var KC_DOWN = 40;
-
 var KC_W = 87;
 var KC_A = 65;
 var KC_S = 83;
 var KC_D = 68;
-
+var KC_J = 74;
 var KC_SPACE = 32;
 var KC_SHIFT = 16;
+var KC_ENTER = 13;
 
 var canvasWidth = 800;
 var canvasHeight = 600;
 var stage;
 var FPS = 30;
 var btnPlay, btnInstruct, btnMenu, btnContinue;
-var playingLevelIntro;
-var walkingSprite;
-var titleScreen, instructionScreen, gameplayScreen, gameOverScreen, levelSign;
-var titleContainer, instructionContainer, gameplayContainer, gameOverContainer, levelDisplayContainer;
+var titleScreen, instructionScreen, gameplayScreen, gameOverScreen;
+var titleContainer, instructionContainer, gameplayContainer, gameOverContainer;
 var gameState;
-var frameCount, gameTimer, gameScore;
-var gameTimerLimit = 10;
+var frameCount, gameTimer;
 var mouseX, mouseY;
-var gameTimeText, gameScoreText, finalScoreText, mouseCoordText, levelText;
 var GameStates = { gameTitle:0, gameInstructions:1, gamePlay:2, gameOver:3};
 var queue;
 manifest = [
@@ -38,8 +34,6 @@ manifest = [
     {src:"gameOverScreen.jpg", id:"gameOverScreen"},
     {src:"gameplayArea.jpg", id:"gameplayScreen"},
     {src:"buttons.png", id:"button"},
-    {src:"sprites.png", id:"walkingSprites"},
-    {src:"levelSign.png", id:"levelSign"}
 ];
 
 /*------------------------------Setup------------------------------*/
@@ -72,7 +66,6 @@ function loadComplete(evt)
     instructionScreen = new createjs.Bitmap(queue.getResult("instructionScreen"));
     gameplayScreen = new createjs.Bitmap(queue.getResult("gameplayScreen"));
     gameOverScreen = new createjs.Bitmap(queue.getResult("gameOverScreen"));
-    levelSign = new createjs.Bitmap(queue.getResult("levelSign"));
     var buttonSheet = new createjs.SpriteSheet({
         images: [queue.getResult("button")],
         frames: {width: 93, height: 33, regX: 46, regY: 15},
@@ -96,31 +89,11 @@ function loadComplete(evt)
     btnMenu = new createjs.Sprite(buttonSheet);
     btnContinue = new createjs.Sprite(buttonSheet);
     
-    var walkSheet = new createjs.SpriteSheet({
-        images: [queue.getResult("walkingSprites")],
-        frames: [[160,0,19,49,0,10.05,48.6],[179,0,34,44,0,17.05,43.6],[213,0,22,46,0,9.05,45.6],[235,0,17,49,0,8.05,48.6],[0,49,25,49,0,10.05,48.6],[25,49,31,46,0,14.05,45.6],[56,49,33,44,0,16.05,43.6],
-                 [89,49,30,44,0,17.05,43.6],[119,49,28,46,0,17.05,45.6],[147,49,19,49,0,10.05,48.6],[166,49,23,49,0,14.05,48.6],[189,49,31,46,0,16.05,45.6],[220,49,34,44,0,17.05,43.6],[0,98,19,49,0,9.05,48.6],
-                 [19,98,34,44,0,17.05,43.6],[53,98,22,46,0,13.05,45.6],[75,98,17,49,0,9.05,48.6],[92,98,25,49,0,15.05,48.6],[117,98,31,46,0,17.05,45.6],[148,98,33,44,0,17.05,43.6],[181,98,30,44,0,13.05,43.6],
-                 [211,98,28,46,0,11.05,45.6],[0,147,19,49,0,9.05,48.6],[19,147,23,49,0,9.05,48.6],[42,147,31,46,0,15.05,45.6],[73,147,34,44,0,17.05,43.6]],
-        animations: {
-            standRight: [0, 0, "standRight"],
-            walkRight: [1, 12, "walkRight", .5],
-            standLeft: [13, 13, "standLeft"],
-            walkLeft: [14, 25, "walkLeft", .5]
-            }     
-        });
-    
-    walkingSprite = new createjs.Sprite(walkSheet);
-    
     setupButtons();
     setupTitleScreen();
     setupGameOverScreen();
     setupGameplayScreen();
     setupInstructionScreen();
-    mouseCoordText = new createjs.Text("X: \nY: ", "12px Arial", "#ffffff");
-    mouseCoordText.x = 20;
-    mouseCoordText.y = canvasHeight-50;
-    stage.addChild(mouseCoordText);
 }
 function setupButtons()
 {
@@ -153,7 +126,6 @@ function setupButtons()
 //region Main
 function loop()
 {
-    mouseCoordText.text = "X: " + mouseX +"\nY: " + mouseY;
     gameStateAction();
     stage.update();
 }
@@ -183,8 +155,10 @@ function handleKeyDown(evt)
         case KC_A: console.log("A ("+evt.keyCode+") down"); return false;
         case KC_S: console.log("S ("+evt.keyCode+") down"); return false;
         case KC_D: console.log("D ("+evt.keyCode+") down"); return false;
+        case KC_J: console.log("J ("+evt.keyCode+") down"); return false;
         case KC_SPACE:  console.log("SPACE ("+evt.keyCode+") down"); return false;
         case KC_SHIFT:  console.log("SHIFT ("+evt.keyCode+") down"); return false;
+        case KC_ENTER:  console.log("ENTER ("+evt.keyCode+") down"); return false;
     }
 }
 
@@ -201,8 +175,10 @@ function handleKeyUp(evt)
         case KC_A:	console.log("A ("+evt.keyCode+") up"); break;
         case KC_S:	console.log("S ("+evt.keyCode+") up"); break;
         case KC_D:	console.log("D ("+evt.keyCode+") up"); break;
+        case KC_J:  console.log("J ("+evt.keyCode+") up"); break;
         case KC_SPACE:	console.log("SPACE ("+evt.keyCode+") up"); break;
         case KC_SHIFT:	console.log("SHIFT ("+evt.keyCode+") up"); break;
+        case KC_ENTER:  console.log("ENTER ("+evt.keyCode+") up"); break;
     }
 }
 
@@ -237,17 +213,11 @@ function gameStateAction()
             
         break;
         case GameStates.gamePlay:
-            if(!playingLevelIntro)
-            {
-                runGameTimer();
-                walkingSprite.x +=1;
-                gameTimeText.text = "" + gameTimer.toFixed();
-                if(gameTimer >= gameTimerLimit)
-                {
-                    gameState = GameStates.gameOver;
-                    walkingSprite.stop();
-                }
-            }
+            runGameTimer();
+            //if(end game condition)
+            //{
+            // gameState = GameStates.gameOver;
+            //}
         break;
         case GameStates.gameInstructions:
             
@@ -261,7 +231,6 @@ function gameStateSwitch()
     switch(gameState)
     {
         case GameStates.gameOver:
-            finalScoreText = "Score: " +gameScoreText;
             titleContainer.visible = false;
             instructionContainer.visible = false;
             gameplayContainer.visible = false;
@@ -331,67 +300,14 @@ function setupInstructionScreen()
 //region Title
 function setupGameplayScreen()
 {
-    gameTimeText = new createjs.Text("10", "50px Arial", "#ffffff");
-    gameTimeText.x = canvasWidth - 80;
-    gameTimeText.y = 50;
-    
-    gameScoreText = new createjs.Text("0", "50px Arial", "#ffffff");
-    gameScoreText.x = 50;
-    gameScoreText.y = 50;
-    
-    walkingSprite.x=10;
-    walkingSprite.y=530;
-    //walk.gotoAndPlay("walkRight");
-    
-    setupLevelDisplay();
-    
     gameplayContainer = new createjs.Container();
-    gameplayContainer.addChild(gameplayScreen, gameTimeText, gameScoreText, walkingSprite, levelDisplayContainer);
+    gameplayContainer.addChild(gameplayScreen);
     stage.addChild(gameplayContainer);
     gameplayContainer.visible = false;
-}
-function setupLevelDisplay()
-{
-    levelSign.x = canvasWidth/2;
-    levelSign.y = canvasHeight/2;
-    
-    levelText = new createjs.Text("0", "80px Arial", "#ffffff");
-    levelText.textAlign = "center";
-    levelText.x = canvasWidth/2+220;
-    levelText.y = canvasHeight/2+130;
-    
-    levelDisplayContainer = new createjs.Container();
-    levelDisplayContainer.addChild(levelSign, levelText);
-    levelDisplayContainer.visible = false;
 }
 function startGameplay()
 {
     resetGameTimer();
-    gameScore = 0;
-    walkingSprite.x=10;
-    levelDisplayContainer.regX = levelDisplayContainer.getBounds().width/2;
-    levelDisplayContainer.regY = levelDisplayContainer.getBounds().height/2;
-    levelDisplayContainer.x = 0;
-    levelDisplayContainer.y = -canvasHeight;
-    levelDisplayContainer.visible = true;
-    tweenLevel();
-}
-var levelTween;
-function tweenLevel()
-{
-    playingLevelIntro = true;
-    levelTween = createjs.Tween.get(levelDisplayContainer, {loop:false})
-        .wait(500)
-        .to({x:0, y:0, rotation:0}, 2000, createjs.Ease.bounceOut)
-        .wait(500)
-        .to({y:canvasHeight+200, rotation:0}, 1000, createjs.Ease.backIn)
-        .call(levelTweenComplete);
-}
-function levelTweenComplete()
-{
-    playingLevelIntro = false;
-    levelDisplayContainer.visible = false;
-    walkingSprite.gotoAndPlay("walkRight");
 }
 //endregion
 /*----------------------------Game Over----------------------------*/
@@ -401,13 +317,8 @@ function setupGameOverScreen()
     btnContinue.x = canvasWidth/2;
     btnContinue.y = canvasHeight/2+100;
     
-    finalScoreText = new createjs.Text("Score: 0", "40px Arial", "#ffffff");
-    finalScoreText.textAlign = "center"
-    finalScoreText.x = canvasWidth/2;
-    finalScoreText.y = canvasHeight/2;
-    
     gameOverContainer = new createjs.Container();
-    gameOverContainer.addChild(gameOverScreen, btnContinue, finalScoreText);
+    gameOverContainer.addChild(gameOverScreen, btnContinue);
     stage.addChild(gameOverContainer);
     gameOverContainer.visible = false;
 }
